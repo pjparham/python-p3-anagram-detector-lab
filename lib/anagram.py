28ba21af08a63cc49e0d74836146081b2bb80648
Parker Parham <parkerparham@gmail.com>
2023-02-07 14:59:35 -0500

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        new_words = [word for word in words if len(word) == len(self.word)]
+        result = []
+        for word in new_words:
+            word_letters = [letter for letter in word]
+            self_letters = [letter for letter in self.word]
+            if sorted(word_letters) == sorted(self_letters):
+                result.append(word)
+        return result
